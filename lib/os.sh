1075ce19401386255a1c8bfb1d2d51ad01f84a8c
Egor Tensin <egor@tensin.name>
2026-09-21 22:24:19 +0500

    remove annoying trailing period from repo URLs

--- lib/os.sh
+++ lib/os.sh
@@ -1,6 +1,6 @@
 # Copyright (c) 2016 Egor Tensin <egor@tensin.name>
 # This file is part of the "Config file sharing" project.
-# For details, see https://github.com/egor-tensin/config-links.
+# For details, see https://github.com/egor-tensin/config-links
 # Distributed under the MIT License.
 
 # Mostly Cygwin-related stuff
